b2e1cacebfd5a6d0fd89cda9c145995bad351a6e
junio <rogerio.junior30@escola.pr.gov.br>
2026-03-17 14:41:54 +0000

    versao2

--- hora.js
+++ hora.js
@@ -1,3 +1,10 @@
 //Valores iniciais do Dia
 let ValorPorHora = 50;
 let horasTrabalhadas = 8;
+
+console.log("---Resumo do Expediente---");
+console.log(`Horas iniciais: ${horasTrabalhadas}h. `);
+
+//git add hora.js
+//git commit -m "versao2"
+//git push
