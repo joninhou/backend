785c7025a530b09114ad595f3b0e91d0f89d0973
junio <rogerio.junior30@escola.pr.gov.br>
2026-03-24 14:42:12 +0000

    versao1

---
+++ interpolacao.js
@@ -0,0 +1,10 @@
+const nome = "Bela";
+const idade = 16;
+const anoAtual = "2026";
+
+const anoNascimento = anoAtual - idade;
+const idadeEmDezAnos = idade + 10;
+
+console.log("O ano de nascimento é: " + anoNascimento);
+console.log(`${nome} possui ${idade} anos.`);
+console.log (`Daqui a 10 anos, terá ${idadeEmDezAnos} anos.`);
