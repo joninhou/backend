e9ac5355b468956cb6a11a9c7e3aab78d8586429
junio <rogerio.junior30@escola.pr.gov.br>
2026-04-06 13:48:39 +0000

    versao1

---
+++ saudacao.js
@@ -0,0 +1,23 @@
+//Declaração com parâmetro
+function saudacao(nomeJogador) {
+    console.log("Bem vindo ao jogo, " + nomeJogador + "!")
+};
+
+saudacao("Júnior Neymah");
+
+//Função que recebe um valor, soma 1 número e retorna
+function subirDeNivel(nivelAtual) {
+    return nivelAtual + 1
+};
+
+let meuNivel = 10;
+
+let nivelAtualizado = subirDeNivel(meuNivel);
+console.log("Seu nível atual é: " + nivelAtualizado);
+
+//Função que soma dois números
+function soma(numero1, numero2) {
+    return numero1 * numero2
+};
+
+console.log(soma(8,8))
