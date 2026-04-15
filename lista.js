d45fdacf979ebd0abddf005423b2bf3a74b5b41c
junio <rogerio.junior30@escola.pr.gov.br>
2026-04-15 13:44:13 +0000

    versao1

---
+++ lista.js
@@ -0,0 +1,11 @@
+const alunos = ["Bela", "Mio", "chu"];
+
+alunos.push("Maria");
+alunos.unshift("pikkachu");
+console.log(alunos);
+
+console.log(alunos.indexOf("Maria"));
+console.log(alunos.indexOf("Iwata"));
+
+console.log(alunos.includes("Bibia"));
+console.log(alunos.includes("Belinha"));
