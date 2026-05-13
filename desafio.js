be2e073c8ad629074a5e894fc20faa0772f940b2
junio <rogerio.junior30@escola.pr.gov.br>
2026-05-13 13:49:36 +0000

    v1

---
+++ desafio.js
@@ -0,0 +1,12 @@
+//Declarar 5 variáveis para representar o perfil de um jogador
+
+//Fazer uma saída de forma interpolada (`${}`)
+
+const nome = "Yamal";
+const idade = 18;
+const posicao = "Ponta-direita";
+const time = "Barcelona";
+const altura = 1.80;
+const pesoEmKilos = 72;
+
+console.log(`O jogador ${nome}, segue atuando como ${posicao} no time espanhol ${time}. E com apenas ${idade} anos de idade, já se encontra em tamanho adulto com ${altura} de altura e ${pesoEmKilos} quilos.`)
